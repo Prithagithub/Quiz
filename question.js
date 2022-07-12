96e9032c418a51df43ba77b3a230c826b49503ca
Pritha Roy Chowdhury <108894909+Prithagithub@users.noreply.github.com>
2022-07-12 21:22:39 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The full form of CSS is:",
+    answer: "Cascading Style Sheet",
+    options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following tag is used to embed css in html page?",
+    answer: "style",
+    options: [
+      "css",
+      "style",
+      "!DOCTYPE html",
+      "script"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following CSS selectors are used to specify a group of elements?",
+    answer: "class",
+    options: [
+      "tag",
+      "class",
+      "id",
+      "both class and tag"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following has introduced text, list, box, margin, border, color, and background properties?",
+    answer: "CSS",
+    options: [
+      "HTML",
+      "PHP",
+      "Ajax",
+      "CSS"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "How can you created rounded corners using CSS3?",
+    answer: "border[round]: 30px",
+    options: [
+      "border[round]: 30px",
+      "corner-effect: round",
+      "border-radius: 30px",
+      "alpha-effect: round-corner"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following CSS framework is used to create a responsive design?",
+    answer: "bootstrap",
+    options: [
+      "django",
+      "larawell",
+      "rails",
+      "bootstrap"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following CSS property is used to make the text bold?",
+    answer: "font-weight: bold",
+    options: [
+      "font-weight: bold",
+      "font-style: bold",
+      "text-align: bold",
+      "text-decoration: bold"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following CSS style property is used to specify an italic text?",
+    answer: "font-style",
+    options: [
+      "style",
+      "font-style",
+      "font",
+      "All of These"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following are the CSS Extension Prefixes for Webkit?",
+    answer: "-webkit",
+    options: [
+      "-webkit",
+      "-o-",
+      " -web",
+      " -chrome"
+    ]
+  },
+];
